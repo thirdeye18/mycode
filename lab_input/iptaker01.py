c0cafe8df66b9e176079aa0bd5c8b04f56edb551
Justin Hammel <student@bchd.86cfa41c-4da3-4c29-a171-55afe98d5bce>
2021-06-28 02:09:49 +0000

    learning about input statement

---
+++ lab_input/iptaker01.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+user_input=input("Please enter an ipv4 IP Adress:")
+print("You told me the IPv4 IP Adress:"+user_input)
+
---
+++ lab_input/iptaker02.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+user_input = input("Please enter an IPv4 IP address:")
+
+## the line below creates a single string that is passed to print()
+# print("You told me the IPv3 address is:" + user_input
+
+## print() can be given a series of objects seperated by a comma
+print("You told me the IPv4 IP address:", user_input)
+
+vendor_input = input("Please enter the vendor name associated with the device.")
+print("You told me the vendor name was ", vendor_input)
