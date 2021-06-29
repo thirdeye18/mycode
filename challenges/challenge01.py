7800eddeda9828fda9483991acaa363574263991
Justin Hammel <student@bchd.86cfa41c-4da3-4c29-a171-55afe98d5bce>
2021-06-29 21:39:52 +0000

    split functions into seperate files and then try to import them to another program (not working)

--- challenges/challenge01.py
+++ challenges/challenge01.py
@@ -1,35 +1,12 @@
 #!/usr/bin/env python3
 
+# import all functions from my other file
+from  myFunctions import *
+
 ## Does the number 6 appear?
 ## Given a list of ints determine if the number 6 is in the list.
 
-## This function receives input from user and puts it into a list
-## values must be seperated by a space
-## pressing enter stops list input
-def createList():
-    # Instructions for the user
-    print("Please input a list of integers to test. Follow each value with a space.\nPress enter following the last number to continue") 
-    
-    # take multiple inputs into list, iteration with for loop until enter pressed
-    userTestList = [ int(x) for x in input().split()]
-    # printing the users list  
-    print("List to test:", userTestList)
-    return userTestList
+# createList passed to intSearch  
+returned_list = createList()
+intSearch(returned_list)
 
-# Function to test the presence of a specified int
-def intSearch(userTestList):
-    searchValue = int(input("What number are you looking for? "))
-    print("You are looking for:", searchValue)
-    if searchValue in userTestList:
-        print(searchValue, "is in the list")
-    else:
-        print(searchValue, "is not in the list")
-
-# Start of main program calling functions defined above
-def main():
-    # use creatList() to generate testList
-    #testlist = createList()
-    #intSearch(testlist2)
-    intSearch(createList())
-
-main()
